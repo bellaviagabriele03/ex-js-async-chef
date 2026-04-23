bee6921feb5b1fa3cd19bf83edaac4fc5d84785d
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-04-23 18:35:05 +0200

    Feat: chiamate senza gestione degli errori

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-In questo esercizio, utilizzerai async / await per creare la funzione getChefBirthday(id).Questa funzione accetta un id di una ricetta e deve:
-Recuperare la ricetta da https://dummyjson.com/recipes/{id}
-Estrarre la proprietà userId dalla ricetta
-Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
-Restituire la data di nascita dello chef
-Note del docente
-Scrivi la funzione getChefBirthday(id), che deve:
-Essere asincrona(async).
-Utilizzare await per chiamare le API.
-Restituire una Promise con la data di nascita dello chef.
-Gestire gli errori con try/catch
+// In questo esercizio, utilizzerai async / await per creare la funzione 
+// getChefBirthday(id).Questa funzione accetta un id di una ricetta e deve:
+// Recuperare la ricetta da https://dummyjson.com/recipes/{id}
+// Estrarre la proprietà userId dalla ricetta
+// Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
+// Restituire la data di nascita dello chef
+// Note del docente
+// Scrivi la funzione getChefBirthday(id), che deve:
+// Essere asincrona(async).
+// Utilizzare await per chiamare le API.
+// Restituire una Promise con la data di nascita dello chef.
+// Gestire gli errori con try/catch
+
+
+//funzione di supporto per trasformare il mio oggetto json
+async function fetchTransform(url) {
+    const response = await fetch(url)
+    const obj = response.json();
+    return obj
+
+}
+
+
+
+
+
+
+async function getChefBirthday(id) {
+    const recipe = await fetchTransform(`https://dummyjson.com/recipes/${id}`)
+    const user = await fetchTransform(`https://dummyjson.com/users/${recipe.userId}`)
+    return user.birthDate
+
+}
+
+
+
+
+
+
+
+getChefBirthday(1)
+    .then(birthday => console.log("data compleanno dello chef:", birthday))
+    .catch(error => console.error(error))
